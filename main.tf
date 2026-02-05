c445c9ed8feff8d5b5d6d08fe8419a199a989ce5
G4F411 <160422155+G4F411@users.noreply.github.com>
2026-02-05 18:16:07 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "docker_image" "nginx_image" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx-iac-lab"
+  image = docker_image.nginx_image.image_id
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
